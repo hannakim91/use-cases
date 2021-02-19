af04e8dd25e865c621ce36cb02fdc8c597b29223
Hanna Kim <hannakim91@gmail.com>
2021-02-19 15:43:49 -0700

    Add all necessary inputs fields

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,17 +5,17 @@ const Form = (props) => (
   <div>
     <h1>Anywhere in your app!</h1>
     <Formik
-      initialValues={{ role: '', businessType: '' }}
+      initialValues={{ role: '', businessType: '', productName: '', triggerEvent: '', useCase: ''}}
       validate={values => {
         const errors = {};
-        if (!values.role || !values.businessType) {
+        if (!values.role || !values.businessType || !values.productName || !values.triggerEvent || !values.useCase) {
           errors.role = 'All inputs required';
         } 
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
+          alert(JSON.stringify(values, null, 6));
           props.saveRole(values.role)
           setSubmitting(false);
           // part of Formik component 
@@ -50,6 +50,30 @@ const Form = (props) => (
             value={values.businessType}
           />
           {errors.businessType && touched.businessType && errors.businessType}
+          <input
+            type="productName"
+            name="productName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.productName}
+          />
+          {errors.productName && touched.productName && errors.productName}
+          <input
+            type="triggerEvent"
+            name="triggerEvent"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.triggerEvent}
+          />
+          {errors.triggerEvent && touched.triggerEvent && errors.triggerEvent}
+          <input
+            type="useCase"
+            name="useCase"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.useCase}
+          />
+          {errors.useCase && touched.useCase && errors.useCase}
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
