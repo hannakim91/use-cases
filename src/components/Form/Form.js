9f5646a2821fc855e311d48367c7e6ca2fa060d8
Hanna Kim <hannakim91@gmail.com>
2021-02-20 06:39:13 -0700

    Add basic validation - all fields required

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
 const Form = (props) => (
   <div>
     <h1>Form</h1>
     <Formik
       initialValues={{ role: '', businessType: '', productName: '', triggerEvent: '', useCase: ''}}
-      validate={values => {
-        const errors = {};
-        if (!values.role || !values.businessType || !values.productName || !values.triggerEvent || !values.useCase) {
-          errors.role = 'All inputs required';
-        } 
-        return errors;
-      }}
+      // validate={values => {
+      //   const errors = {};
+      //   if (!values.role || !values.businessType || !values.productName || !values.triggerEvent || !values.useCase) {
+      //     errors.role = 'All inputs required';
+      //   } 
+      //   return errors;
+      // }}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 6));
@@ -22,6 +23,14 @@ const Form = (props) => (
             // eg loading... 
         }, 400);
       }}
+
+      validationSchema={yup.object().shape({
+        role: yup.string().required("Role is required"),
+        businessType: yup.string().required("Business type is required"),
+        productName: yup.string().required("Product name is required"),
+        triggerEvent: yup.string().required("Trigger/event is required"),
+        useCase: yup.string().required("Use case is required")
+      })}
     >
       {({
         values,
